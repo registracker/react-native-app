c55a54d4341871291b7ed9a7d05b42f0c658749d
Miguel Hernández <miguel.hernandezp@salud.gob.sv>
2023-01-06 15:13:16 -0600

    wip: get position

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text } from 'react-native'
+import Geolocation from 'react-native-geolocation-service';
 
 
 export const Home = () => {
+
+    const [position, setPosition] = useState()
+
+
+    Geolocation.watchPosition(
+        (position) => {
+            setPosition(position)
+        },
+        (error) => {
+            // See error code charts below.
+            console.log(error.code, error.message);
+        },
+        { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+    );
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>
-                home
+                {JSON.stringify(position, null, 5)}
             </Text>
         </View>
     )
