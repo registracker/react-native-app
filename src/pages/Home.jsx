12f3a6cb87e62edce3cb0c41f20242437dce96d5
Miguel Hernández <miguel.hernandezp@salud.gob.sv>
2023-01-10 15:30:16 -0600

    wip: historial de puntos de gps

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,87 @@
-import React, { useState } from 'react'
-import { View, Text } from 'react-native'
-import Geolocation from 'react-native-geolocation-service';
+import { format } from 'date-fns/esm';
+import React, { useEffect, useState } from 'react'
+import { View, Text, Button } from 'react-native'
+import Geolocation, { stopObserving } from 'react-native-geolocation-service';
 
 
 export const Home = () => {
 
+    const [time, setTime] = useState(format(new Date(), 'yyyy-MM-dd HH:mm:ss'))
+    const [data, setData] = useState([])
     const [position, setPosition] = useState()
 
 
-    Geolocation.watchPosition(
-        (position) => {
-            setPosition(position)
-        },
-        (error) => {
-            // See error code charts below.
-            console.log(error.code, error.message);
-        },
-        { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-    );
+    const getLocation = () => {
+
+
+
+        Geolocation.getCurrentPosition(
+            (position) => {
+                setPosition(position)
+                console.log(format(new Date(position.timestamp), 'yyyy-MM-dd HH:mm:ss'), position.coords.latitude, position.coords.longitude);
+            },
+            (error) => {
+                // See error code charts below.
+                console.error(error.code, error.message);
+            },
+            {
+                enableHighAccuracy: true,
+                distanceFilter: 25,
+                timeout: 15000,
+                maximumAge: 10000
+            }
+        )
+
+        return position;
+    }
+
+    const getLocationObservation = async () => {
+
+        const observation = await Geolocation.watchPosition(
+            (position) => {
+                setPosition(position)
+                setData([...data, position])
+                console.log('WATCH', format(new Date(position.timestamp), 'yyyy-MM-dd HH:mm:ss'), position.coords.latitude, position.coords.longitude);
+            },
+            (error) => {
+                // See error code charts below.
+                console.error(error.code, error.message);
+            },
+            {
+                enableHighAccuracy: true,
+                // distanceFilter: 25,
+                // timeout: 15000,
+                // maximumAge: 10000
+                interval: 5000,
+            }
+        )
+
+    }
+
+
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>
                 {JSON.stringify(position, null, 5)}
             </Text>
+
+                <Button
+                    title='Get Location'
+                    onPress={getLocation}
+                />
+
+                <Button
+                    title='Observar'
+                    onPress={getLocationObservation}
+                />
+
+
+                <Text>
+                    {JSON.stringify(position, null, 5)}
+                </Text>
         </View>
     )
 }
+
+
+
